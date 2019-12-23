5c307eb21206f61cd31f5cccbbfbe8726deab3da
Kristina-zh <kristina.zhuravleva1991@gmail.com>
2019-12-23 16:50:33 +0200

    Updates

---
+++ precache-manifest.4a717eaab160278cc975b62fa77cd542.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc03fe925b7fa27e607dd313d2cbe1e5",
+    "url": "/Platform195/index.html"
+  },
+  {
+    "revision": "7bb2f81d1756650f7c77",
+    "url": "/Platform195/static/css/main.699eed31.chunk.css"
+  },
+  {
+    "revision": "2a01e24a2b747d35cabb",
+    "url": "/Platform195/static/js/2.cb939bd8.chunk.js"
+  },
+  {
+    "revision": "7bb2f81d1756650f7c77",
+    "url": "/Platform195/static/js/main.a77b6f7a.chunk.js"
+  },
+  {
+    "revision": "fbcd1fbd7449facf6ad8",
+    "url": "/Platform195/static/js/runtime-main.c420199f.js"
+  },
+  {
+    "revision": "1535f8ac780c4ef2a6a5f45905cf1093",
+    "url": "/Platform195/static/media/Alan.1535f8ac.jpeg"
+  },
+  {
+    "revision": "cef7bb396109f8d84d186a723f981bd3",
+    "url": "/Platform195/static/media/Anthony.cef7bb39.jpg"
+  },
+  {
+    "revision": "43f65ead3df72caef1bc0a0a80aaf3f0",
+    "url": "/Platform195/static/media/Mark.43f65ead.jpg"
+  },
+  {
+    "revision": "3ad8956e8cb48807b33049e2fe98328c",
+    "url": "/Platform195/static/media/PLATFORM_195_LOGO_white.3ad8956e.jpg"
+  },
+  {
+    "revision": "0e1a20f4220a58e1fcfbb4d14e4224ea",
+    "url": "/Platform195/static/media/PLATFORM_195_reversed.0e1a20f4.jpg"
+  },
+  {
+    "revision": "9168f7f3a899177d386fc966bfe4b709",
+    "url": "/Platform195/static/media/Stuart.9168f7f3.jpg"
+  },
+  {
+    "revision": "49bd157ad95c517b7ca674466332c285",
+    "url": "/Platform195/static/media/facebook.49bd157a.svg"
+  },
+  {
+    "revision": "c057d500fd72706e52f87b057eaeb883",
+    "url": "/Platform195/static/media/instagram.c057d500.svg"
+  },
+  {
+    "revision": "b6d9ba70a9327dd1a5894c7141563f5e",
+    "url": "/Platform195/static/media/linkedin.b6d9ba70.svg"
+  },
+  {
+    "revision": "6134b234fbe588b9dc309b2bd14caa35",
+    "url": "/Platform195/static/media/twitter.6134b234.svg"
+  },
+  {
+    "revision": "dcf3a68ee6f2dc3dc5aa83d7400ef511",
+    "url": "/Platform195/static/media/txt1.dcf3a68e.png"
+  },
+  {
+    "revision": "daf3ef79238f10b4230bf86d471732ca",
+    "url": "/Platform195/static/media/txt2.daf3ef79.png"
+  },
+  {
+    "revision": "b1748a47e124702fa4153cc64429124b",
+    "url": "/Platform195/static/media/txt3.b1748a47.png"
+  },
+  {
+    "revision": "3ebfae65cd6ffebed385a5bbf11ef209",
+    "url": "/Platform195/static/media/vimeo.3ebfae65.svg"
+  }
+]);
